74f75931fc2f3ff6fc7669a63912c6b5569a529c
Shree kant Das <shreekantdas@pop-os.localdomain>
2020-04-25 19:31:27 +0530

    Regressing python

---
+++ regression.py
@@ -0,0 +1,2 @@
+# Data center : https://www.kaggle.com/andonians/random-linear-regression/data
+# https://towardsdatascience.com/introduction-to-machine-learning-algorithms-linear-regression-14c4e325882a
